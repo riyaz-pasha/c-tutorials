1229879a7b8446add7b52354ba150fa674e50a91
riyaz <riyazpasha880@gmail.com>
2021-04-28 19:14:53 +0530

    datatypes . char int float

---
+++ day-2/datatypes.c
@@ -0,0 +1,13 @@
+// Characters/Alphabets a,b,c,d
+// Number 1,2,3,
+#include <stdio.h>
+int main()
+{
+    char gender = 'M'; // varibale = value
+    int age = 23;      //
+    float cgpa = 8.8;
+
+    printf("Gender : %c\n", gender);
+    printf("Age: %d\n", age);
+    printf("CGPA : %f\n", cgpa);
+}
