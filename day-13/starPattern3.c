9acbd94070dda74bb360c228901d6552c9e910d4
riyaz <riyazpasha880@gmail.com>
2021-05-21 18:50:12 +0530

    day 13. print daignol * matrix based on rows with spacces

---
+++ day-13/starPattern3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    //pattern3 - I'll
+    //*****
+    // ****
+    //  ***
+    //   **
+    //    *
+
+    int n = 5;
+    int row = n;
+    while (row > 0) // row=0 0>0 false exits loop
+    {
+        int spaces = n - row; // spaces=5-1=4
+        while (spaces > 0)    // 0>0 false loop exits
+        {
+            printf(" "); // prints space
+            spaces--;    // space=1-1=0
+        }
+
+        int column = row;  // column=1 row=1
+        while (column > 0) // column=0 0>0 false exits loop
+        {
+            printf("*"); // prints *
+            column--;    //column=1-1=0
+        }
+        printf("\n");
+        row--; //row=1-1=0
+    }
+}
+
+//*****
+// ****
+//  ***
+//   **
+//    *
+//
