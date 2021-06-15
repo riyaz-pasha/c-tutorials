87afc2cbd3e39326d316cf8d2381fd274c97d532
riyaz <riyazpasha880@gmail.com>
2021-06-15 18:42:35 +0530

    Reverse of a string

---
+++ day-21/reverseString.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    // Reverse of a string
+    // RIYAZ
+    // RIYA -> Z
+    // RIY -> ZA
+    // RI -> ZAY
+    // R -> ZAYI
+    // -> ZAYIR
+
+    // 1. Find the given string length
+    // {R,I,Y,A,Z,\0}
+    // loop through the string till you find the terminating character 
+
+    // 
+}
